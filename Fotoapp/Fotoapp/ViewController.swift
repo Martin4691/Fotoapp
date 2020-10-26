d6778e4ff5799726a68f6ea461a4bebfec70cf7d
Martín4691 <martinsm1991@gmail.com>
2020-10-26 12:56:23 +0100

    Y a tengo los cuatro iconos de la tab bar. Funcionan correctamente. Ahora hay que ir a implementar cosas en el item 1 Photos.

--- Fotoapp/Fotoapp/ViewController.swift
+++ Fotoapp/Fotoapp/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PicturesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    class PicturesCell: UIViewController {
+        
+    }
 
+    
+  
+        
+    
+//    Fase 2: CollectionView
+//    Agregar collection view al primer view controller
+//    Crear la clase PicturesViewController (hereda de UICollectionViewController)
+//    Crear la clase PicturesCell (hereda de UICollectionViewCell)
+//    Implementar UICollectionViewDataSource en la clase PicturesViewController
+//    Configurar, desde el storyboard:
+//    El item 1 de la tab bar es de tipo PicturesViewController
+//    Los outlets dataSource y el delegate de la collection view están conectados con PicturesViewController
+
+    
 }
 
