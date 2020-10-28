d0bf70185e4fa882dab4c5272c0e803cb2ec6723
Martín4691 <martinsm1991@gmail.com>
2020-10-28 12:31:37 +0100

    ya tenemos las fotos alternativas.

--- Fotoapp/Fotoapp/Pictures/Features/PicturesCollection/PicturesViewController.swift
+++ Fotoapp/Fotoapp/Pictures/Features/PicturesCollection/PicturesViewController.swift
@@ -10,65 +10,86 @@ import UIKit
 
 
 class PicturesViewController: UIViewController {
-    private let maxHorizontalCells: CGFloat = 0.0
-    private let marginBetweenCells: CGFloat = 0.0
-    private let reuseIdentifier = String(describing: PicturesCell.self) // essto sirve para decir: PicturesCell
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    override func viewDidLoad() {
-        let nib = UINib(nibName: reuseIdentifier, bundle: nil)
-        // nib es igual que xib, pero con una nomenclatura antigua.
-        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
-        super.viewDidLoad()
-    }
-    
+  private let maxHorizontalCells: CGFloat = 0.0
+  private let marginBetweenCells: CGFloat = 0.0
+  private let reuseIdentifier = String(describing: PicturesCell.self) // essto sirve para decir: PicturesCell
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    let nib = UINib(nibName: reuseIdentifier, bundle: nil)
+    // nib es igual que xib, pero con una nomenclatura antigua.
+    collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+    super.viewDidLoad()
+  }
+  
 }
 
 // MARK: UICollectionViewDataSource
 
 extension PicturesViewController: UICollectionViewDataSource {
-    // Este método no es necesario.
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
+  // Este método no es necesario.
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return  100
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return  100
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-//        if let pictureCell.previewImage.image = indexPath.row % 2 == 0 ? _ : _
-//    } else {
     cell.backgroundColor = .blue
-//    }
     
-    return cell
+    if let pictureCell = (cell as? PicturesCell) {
+      pictureCell.label.text = "\(indexPath.row + 1)"
+      
+      //         METODO CESC: -------(para acceder a los pares/impares)
+
+      //            pictureCell.image.image = indexPath.row % 2 == 0 ? #imageLiteral(resourceName: "pic1"):#imageLiteral(resourceName: "pic2")
+      
+      //    metodo internet:  -------(para acceder a los pares/impares)
+      if indexPath.row % 2 == 0 {
+        pictureCell.image.image = UIImage(named: "pic1")
+      } else {
+        pictureCell.image.image = UIImage(named: "pic2")
+      }
+      
     }
+    return cell
+  }
 }
 
 extension PicturesViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.item)
-    }
+  func collectionView(_ collectionView: UICollectionView,
+                      didSelectItemAt indexPath: IndexPath) {
+    print(indexPath.item)
+  }
 }
 
 extension PicturesViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
-        return CGSize(width: 75, height: 75)
-    }
-
-    func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumLineSpacingForSectionAt: Int) -> CGFloat {
-        
-        return maxHorizontalCells
-    }
-
-    func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt: Int) -> CGFloat {
-        
-        return marginBetweenCells
-    }
-
-
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
+    return CGSize(width: 75, height: 75)
+  }
+  
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumLineSpacingForSectionAt: Int) -> CGFloat {
+    
+    return maxHorizontalCells
+  }
+  
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt: Int) -> CGFloat {
+    
+    return marginBetweenCells
+  }
+  
+  
 }
+
+
+
+
+
+
+
+
