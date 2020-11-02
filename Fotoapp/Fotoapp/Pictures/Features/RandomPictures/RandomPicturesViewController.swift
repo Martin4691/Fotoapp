57887cf04b904d5bce202d80cb0959dd329685d7
Martín4691 <martinsm1991@gmail.com>
2020-11-02 12:35:36 +0100

    timer para las fotos. implementado correctamente con timer en block.

--- Fotoapp/Fotoapp/Pictures/Features/RandomPictures/RandomPicturesViewController.swift
+++ Fotoapp/Fotoapp/Pictures/Features/RandomPictures/RandomPicturesViewController.swift
@@ -8,30 +8,47 @@
 import UIKit
 
 class RandomPicturesViewController: UIViewController {
-
+    var timer: Timer = Timer()
+    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshPicture()
+        // timer viejo de obj C:
+        //        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(refreshPicture), userInfo: nil, repeats: true)
         
-////          SOLUCION CESC:
-//        let randomNumber: Int = Int.random(in: 0..<ImagesData.numOfImages())
-//        let img: UIImage? = ImagesData.imageForPosition(randomNumber)
-//        imageView.image = img
-//        print("random number: \(randomNumber)")
-//
+        //timer nuevo de Swift:
+        timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true, block: { _ in self.refreshPicture()
+        })
+        
+        //          SOLUCION CESC para imagen aleatoria (tiene que ir dentro de la viewDidLoad):
+        //        let randomNumber: Int = Int.random(in: 0..<ImagesData.numOfImages())
+        //        let img: UIImage? = ImagesData.imageForPosition(randomNumber)
+        //        imageView.image = img
+        //        print("random number: \(randomNumber)")
+    }
+    
+    //          SOLUCION MIA para imagen aleatoria:
+    func getRandomPictures() -> UIImage? {
+        let randomImage = ImagesData.imageForPosition(Int.random(in: 0...ImagesData.numOfImages()))
+        print("image number: \(randomImage)")
+        return randomImage
+    }
+    //    imageView.image = getRandomPictures()
+    //    (esta es la llamda a mi solucion para imagen aleatoria).
+    
+    // timer viejo de obj C:
+    //    @objc func refreshPicture() {
+    //        imageView.image = getRandomPictures()
+    //    }
+    
+    //timer nuevo de Swift:
+    func refreshPicture() {
         imageView.image = getRandomPictures()
     }
     
-    //          SOLUCION MIA:
-    func getRandomPictures() -> UIImage? {
-              let randomImage = ImagesData.imageForPosition(Int.random(in: 0...ImagesData.numOfImages()))
-              print("image number: \(randomImage)")
-              return randomImage
-              }
-//    imageView.image = getRandomPictures()
-//
-
-
+    
+    
 }
 
