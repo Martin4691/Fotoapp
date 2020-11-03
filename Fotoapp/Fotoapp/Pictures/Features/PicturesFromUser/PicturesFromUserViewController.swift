3fa41949fb028ab6efa55fc49be532044df541b7
Martín4691 <martinsm1991@gmail.com>
2020-11-03 13:31:28 +0100

    third screen finished

--- Fotoapp/Fotoapp/Pictures/Features/PicturesFromUser/PicturesFromUserViewController.swift
+++ Fotoapp/Fotoapp/Pictures/Features/PicturesFromUser/PicturesFromUserViewController.swift
@@ -6,37 +6,57 @@
 //
 
 import UIKit
+import Photos
 
-class PicturesFromUserViewController: UIViewController {
-
+class PicturesFromUserViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var image: UIImageView!
+    var imagePicker: UIImagePickerController = UIImagePickerController()
     
     @IBOutlet weak var buttonOut: UIButton!
     
     @IBAction func buttonAct(_ sender: Any) {
-        print("Elige Foto Button Push")
+        print("Button Push")
+        func openGallary()
+        {
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        openGallary()
     }
-    
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imageCameraRoll = info[.editedImage] as? UIImage
+        image.image = imageCameraRoll
+        dismiss(animated: true)
+    }
+
+    //      //SOLUCION CESC PARA IMAGE PICKER CONTROLLER:
+    //        if UIImagePickerController.isSourceTypeAviable(.savedPhotosAlbum) {
+    //            imagePicker.delegate = self
+    //            imagePicker.sourceType = .savedPhotosAlbum
+    //            imagePicker.allowsEditing = false
+    //
+    //            present(imagePicker, animated: true, completion: nil)
+    //        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         buttonOut.layer.cornerRadius = 10
-     
         image.image = UIImage(named: "pic1")
-  
         
     }
-    
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
