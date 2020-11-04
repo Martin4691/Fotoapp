2796a43b653290dcadf5aa5a535354a2e5b3fb3f
Martín4691 <martinsm1991@gmail.com>
2020-11-04 15:50:52 +0100

    label y todo xD

--- Fotoapp/Fotoapp/Pictures/Features/Common/ImagesData.swift
+++ Fotoapp/Fotoapp/Pictures/Features/Common/ImagesData.swift
@@ -1,6 +1,9 @@
 //
+//  ImagesData.swift
+//  photos
 //
-//
+//  Created by Francesc Navarro on 26/10/2020.
+//  Copyright © 2020 Bamboo Academy. All rights reserved.
 //
 
 import Foundation
@@ -9,6 +12,7 @@ import UIKit
 class ImagesData {
     static var imagesUrl: [URL]? = Bundle.main.urls(forResourcesWithExtension: "jpg", subdirectory: nil)
     private static var likedImages: [Bool] = [Bool](repeating: false, count: numOfImages())
+    private static var imagesTitle: [String] = [String](repeating: "Foto sin título", count: numOfImages())
     
     static func numOfImages() -> Int {
         guard let pathUrls: [URL] = imagesUrl else { return 0 }
@@ -31,26 +35,34 @@ class ImagesData {
         return nil
     }
     
+    // MARK: - Like and Dislike
     static func likeImageAtPosition(_ position: Int) {
-        let arrayIndex: Int = position % numOfImages()
-        if arrayIndex < numOfImages() {
-            likedImages[position] = true
-        }
+        let arrayIndex: Int = safeIndex(position)
+        likedImages[arrayIndex] = true
     }
     
     static func dislikeImageAtPosition(_ position: Int) {
-        let arrayIndex: Int = position % numOfImages()
-        if arrayIndex < numOfImages() {
-            likedImages[position] = false
-        }
+        let arrayIndex: Int = safeIndex(position)
+        likedImages[arrayIndex] = false
     }
     
     static func likeForPosition(_ position: Int) -> Bool {
-        let arrayIndex: Int = position % numOfImages()
-        if arrayIndex < numOfImages() {
-            return likedImages[arrayIndex]
-        }
-        return false
+        let arrayIndex: Int = safeIndex(position)
+        return likedImages[arrayIndex]
     }
-}
+    
+    // MARK: - Image description
+    static func setTitle(_ title: String, position: Int) {
+        imagesTitle[position] = title
+    }
+    
+    static func getTitle(_ position: Int) -> String {
+        return imagesTitle[position]
+    }
+    
+    // MARK: - Private utils
+    private static func safeIndex(_ position: Int) -> Int {
+        return position % numOfImages()
+    }
 
+}
--- Fotoapp/Fotoapp/Pictures/Features/PicturesCollection/PhotoFullVC.swift
+++ Fotoapp/Fotoapp/Pictures/Features/PicturesCollection/PhotoFullVC.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class PhotoFullVC: UIViewController {
-
+    
     @IBOutlet weak var imageFullScreen: UIImageView!
     
+    @IBOutlet weak var titleOutlet: UITextField!
     
     @IBOutlet weak var closeOutlet: UIButton!
     
@@ -18,12 +19,30 @@ class PhotoFullVC: UIViewController {
         dismiss(animated: true)
     }
     
+    @IBOutlet weak var labelOutlet: UILabel!
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        switchOutlet.isOn = ImagesData.likeForPosition(PicturesViewModel.selectedIndex!)
+        titleOutlet.text = ImagesData.getTitle(PicturesViewModel.selectedIndex!)
+        let favorite: String = switchOutlet.isOn ? "Favorite" : "No Favorite"
+        labelOutlet.text = "\(favorite) \n\(titleOutlet.text!) \n\(PicturesViewModel.selectedIndex! + 1)"
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        ImagesData.setTitle("\(titleOutlet.text!)", position: PicturesViewModel.selectedIndex!)
+        print(titleOutlet.text!)
+    }
+    
     @IBOutlet weak var switchOutlet: UISwitch!
+    
     
     @IBAction func switchAction(_ sender: UISwitch) {
         if switchOutlet.isOn == true {
+            ImagesData.likeImageAtPosition(PicturesViewModel.selectedIndex!)
             print("Switch Is On!")
         } else if switchOutlet.isOn == false {
+            ImagesData.dislikeImageAtPosition(PicturesViewModel.selectedIndex!)
             print("Switch Is Off!")
         }
     }
@@ -33,21 +52,21 @@ class PhotoFullVC: UIViewController {
         super.viewDidLoad()
         closeOutlet.layer.cornerRadius = 20
         imageFullScreen.image = PicturesViewModel.selectedImage
-        
+        titleOutlet.text = "Imagen Nº: \(PicturesViewModel.selectedIndex!)"
         
         closeAction(self)
         
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
