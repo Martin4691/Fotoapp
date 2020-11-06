edd9cdc904662cf52c8d6cc1783068556e125485
Martín4691 <martinsm1991@gmail.com>
2020-11-06 10:05:05 +0100

    Alert done successfully

--- Fotoapp/Fotoapp/Pictures/Features/Email/EmailViewController.swift
+++ Fotoapp/Fotoapp/Pictures/Features/Email/EmailViewController.swift
@@ -6,7 +6,7 @@
 import UIKit
 
 class EmailViewController: UIViewController {
-
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var textView: UITextView!
@@ -23,11 +23,35 @@ class EmailViewController: UIViewController {
     
     @IBAction func saveTouched(_ sender: Any) {
         guard let name = nameTextField.text,
-            let email = emailTextField.text else { return }
+              let email = emailTextField.text else { return }
         
         let fullString: String = "\(name);\(email)"
         UserDefaults.standard.set(fullString, forKey: nameEmailKey)
     }
+    
+    @IBAction func resetAction(_ sender: Any) {
+
+        let refreshAlert = UIAlertController(title: "Are you sure?", message: "All data will be lost.", preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            print("Handle Ok logic here")
+            UserDefaults.standard.removeObject(forKey: self.nameEmailKey)
+            print("Reset Done!")
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+            print("Handle Cancel Logic here")
+            print("Reset Aborted")
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
+        
+        
+        
+        
+    }
+    
+    
 }
 
 
